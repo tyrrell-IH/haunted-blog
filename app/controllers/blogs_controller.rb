b9965d45dfb080cc7c8feea4867095261635e480
tyrrell-IH <gotothertm@gmail.com>
2025-02-22 11:22:23 +0900

    whereに渡す値がオブジェクトになったので、フィールドもuser_idからuserに変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.where(user_id: current_user).or(Blog.published).find(params[:id])
+    @blog = Blog.where(user: current_user).or(Blog.published).find(params[:id])
   end
 
   def new
